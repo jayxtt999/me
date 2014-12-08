16fa7443ac6763e85447dad4558ada5a6f4c8277
home <home@home.com>
2014-12-08 09:28:46 +0800

    Application/Admin/Controller/menuController.php

---
+++ Application/Admin/Controller/menuController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 14-11-29
+ * Time: 下午8:40
+ */
+
+class menuController extends abstractController{
+
+    function indexAction(){
+
+        $this->View()->display();
+    }
+
+
+}
