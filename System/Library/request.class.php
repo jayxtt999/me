0e72d44a295efb4b154f4bb0ff7e4286dbeacb0c
xietaotao <435024179@qq.com>
2015-01-07 18:00:28 +0800

    命名空间
    Signed-off-by: xietaotao <435024179@qq.com>

---
+++ System/Library/request.class.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2015/1/7 0007
+ * Time: ÏÂÎç 5:44
+ */
+
+namespace System\Library;
+
+
+class Request {
+
+    function param(){
+
+
+
+    }
+
+
+    function isAjax(){
+        return isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'XMLHttpRequest';
+    }
+
+
+    function getMethod(){
+        return $_SERVER['REQUEST_METHOD'];
+    }
+
+
+} 
