2bb625084bfc07ac354d184b1aa1dbaf3bceb93b
naix <435024179@qq.com>
2015-03-31 22:55:29 +0800

    修修补补何时了

--- Application/Home/Controller/twitterController.php
+++ Application/Home/Controller/twitterController.php
@@ -32,11 +32,31 @@ class twitterController extends abstractController
         foreach($list as $k=>$v){
             $commentWhere['data'] = $v['id'];
             $lists[$k] = $v;
-            $lists[$k]['comment'] =  db()->Table('comment')->getAll($commentWhere)->done();
+            $lists[$k]['comment'] =  $this->sortOut(db()->Table('comment')->getAll($commentWhere)->done());
         }
         $this->getView()->assign(array('list'=>$lists));
         $this->getView()->display();
     }
+
+    /**
+     * 重置序列
+     * @param $cate
+     * @param int $pid
+     * @param int $level
+     * @return array
+     */
+    public function sortOut($cate,$pid=0,$level=0){
+        $tree = array();
+        foreach($cate as $v){
+            if($v['ref_id'] == $pid){
+                $v['level'] = $level + 1;
+                $tree[] = $v;
+                $tree = array_merge($tree, self::sortOut($cate,$v['id'],$level+1));
+            }
+        }
+        return $tree;
+    }
+
 
 
 } 
