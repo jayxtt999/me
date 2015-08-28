7a33fbb2b2b22adc7568981645b1b60ab42a3296
xietaotao Work <435024179@qq.com>
2015-08-28 08:56:05 +0800

    ..

---
+++ System/Library/Open/qq/API/comm/inc.php
@@ -0,0 +1,2 @@
+<?php die('forbidden'); ?>
+{"appid":"101212806","appkey":"2752d3600c9a43dd1d2801592e1358e6","callback":"http://www.xietaotao.cn/member/open/callback","scope":"get_user_info,add_share,list_album,add_album,upload_pic,add_topic,add_one_blog,add_weibo,check_page_fans,add_t,add_pic_t,del_t,get_repost_list,get_info,get_other_info,get_fanslist,get_idolist,add_idol,del_idol,get_tenpay_addr","errorReport":true,"storageType":"file","host":"localhost","user":"root","password":"root","database":"test"}
